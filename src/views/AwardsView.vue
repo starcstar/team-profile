7c5e8008cb3385b4d65abda8fd86d0ec3bd67a85
L0v3ch4n <61489983+Cuber-Wei@users.noreply.github.com>
2024-09-28 19:03:20 +0800

    获奖界面设计 (#16)

--- src/views/AwardsView.vue
+++ src/views/AwardsView.vue
@@ -1,72 +1,128 @@
 <template>
-  <h1 style="margin: 20px">奖项展示</h1>
   <div id="awards-view">
-    <a-col :span="12">
-      <h3>ACM</h3>
-      <a-timeline style="margin: 0 auto">
-        <a-timeline-item label="2020-04-12" dotColor="#00B42A">
-          The first milestone
-        </a-timeline-item>
-        <a-timeline-item label="2020-05-17">
-          The second milestone
-        </a-timeline-item>
-        <a-timeline-item label="2020-06-22">
-          <template #dot>
-            <IconClockCircle :style="{ fontSize: '12px', color: '#F53F3F' }" />
-          </template>
-          The third milestone
-        </a-timeline-item>
-        <a-timeline-item label="2020-06-22" dotColor="var(--color-fill-4)">
-          The third milestone
-        </a-timeline-item>
-      </a-timeline>
-    </a-col>
-    <a-col span="12">
-      <h3>CTF</h3>
-      <a-timeline style="margin: 0 auto">
-        <a-timeline-item label="2020-04-12">
-          <template #dot>
-            <IconCheck
-              :style="{
-                fontSize: '12px',
-                padding: '2px',
-                boxSizing: 'border-box',
-                borderRadius: '50%',
-                backgroundColor: 'var(--color-primary-light-1)',
-              }"
-            />
-          </template>
-          The first milestone
-        </a-timeline-item>
-        <a-timeline-item label="2020-05-17">
-          <template #dot>
-            <IconCheck
-              :style="{
-                fontSize: '12px',
-                padding: '2px',
-                boxSizing: 'border-box',
-                borderRadius: '50%',
-                backgroundColor: 'var(--color-primary-light-1)',
-              }"
-            />
-          </template>
-        </a-timeline-item>
-        <a-timeline-item label="2020-06-22"
-          >The third milestone
-        </a-timeline-item>
-        <a-timeline-item label="2020-06-22" dotColor="var(--color-fill-4)">
-          The third milestone
-        </a-timeline-item>
-      </a-timeline>
-    </a-col>
+    <a-row>
+      <h1 style="margin: 20px">获奖展示</h1>
+    </a-row>
+    <a-row>
+      <a-col :span="3">
+        <div style="display: flex; flex-direction: column; margin: 20px">
+          <a-checkbox
+            :checked="!!pendingProps.reverse"
+            @change="(v) => onChange({ reverse: v })"
+            style="font-size: 16px"
+          >
+            倒序
+          </a-checkbox>
+        </div>
+      </a-col>
+      <a-col :span="17">
+        <a-tabs default-active-key="1">
+          <a-tab-pane key="1" title="ACM">
+            <div class="tabs">
+              <a-card :bordered="false">
+                <a-row>
+                  <a-timeline
+                    class="timeline"
+                    label-position="relative"
+                    mode="alternate"
+                    v-bind="pendingProps"
+                  >
+                    <template v-if="pendingProps.hasPendingDot" #dot>
+                      <IconFire :style="{ color: '#e70a0a' }" />
+                    </template>
+                    <AwardTimeItem
+                      v-for="item of acmAwardsList"
+                      :key="item.id"
+                      :time="item.time"
+                      :competition="item.competition"
+                      :grade="item.grade"
+                      :pic="item?.pic"
+                    />
+                  </a-timeline>
+                </a-row>
+              </a-card>
+            </div>
+          </a-tab-pane>
+          <a-tab-pane key="2" title="CTF">
+            <div class="tabs">
+              <a-card :bordered="false">
+                <a-row>
+                  <a-timeline
+                    class="timeline"
+                    label-position="relative"
+                    mode="alternate"
+                    v-bind="pendingProps"
+                  >
+                    <template v-if="pendingProps.hasPendingDot" #dot>
+                      <IconFire :style="{ color: '#e70a0a' }" />
+                    </template>
+                    <AwardTimeItem
+                      v-for="item of ctfAwardsList"
+                      :key="item.id"
+                      :time="item.time"
+                      :competition="item.competition"
+                      :grade="item.grade"
+                      :pic="item?.pic"
+                    />
+                  </a-timeline>
+                </a-row>
+              </a-card>
+            </div>
+          </a-tab-pane>
+        </a-tabs>
+      </a-col>
+      <a-col :span="4">
+        <div style="display: flex; flex-direction: column; margin: 20px">
+          <a-link href="https://asuri.club/members/#awards">
+            To Asuri
+            <IconLaunch />
+          </a-link>
+        </div>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script setup>
-import { IconCheck } from "@arco-design/web-vue/es/icon";
+import { onMounted, ref } from "vue";
+import { IconFire, IconLaunch } from "@arco-design/web-vue/es/icon";
+import AwardTimeItem from "@/components/AwardTimeItem.vue";
+
+const pendingProps = ref({
+  pending: "Comming soon...",
+  hasPendingDot: true,
+});
+
+const onChange = (newProps) => {
+  pendingProps.value = {
+    ...pendingProps.value,
+    ...newProps,
+  };
+};
+const acmAwardsList = ref([]);
+const ctfAwardsList = ref([]);
+const loadData = async () => {
+  // 初始化
+  acmAwardsList.value = [];
+  ctfAwardsList.value = [];
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  acmAwardsList.value = require("/public/awards/ACM.json");
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  ctfAwardsList.value = require("/public/awards/CTF.json");
+};
+onMounted(() => {
+  loadData();
+});
 </script>
 <style scoped>
 #awards-view {
-  display: flex;
+}
+
+.timeline {
+  width: 100%;
+}
+
+.tabs {
+  margin: 0 auto;
 }
 </style>
--- src/views/IntroductionView.vue
+++ src/views/IntroductionView.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BannerPics from "@/components/BannerPics.vue";
+</script>
 
 <template>
   <div id="introduction-view">
-    <h1 style="margin: 20px">团队介绍</h1>
+    <h1 style="margin: 20px" class="my-h1">团队介绍</h1>
+    <BannerPics style="width: 100%" />
     <div class="container">
       <a-space>
         <h2>General Intro</h2>
@@ -35,5 +38,25 @@
 
 .container {
   padding: 20px;
+}
+
+.my-h1 {
+  //text-decoration: underline;
+  text-underline-color: black;
+  text-underline-offset: 8px;
+}
+
+h1::before {
+  border-radius: 10px;
+  //background: #66666666;
+  content: ">_ ";
+}
+
+h2::before {
+  content: ">/ ";
+}
+
+h3::before {
+  content: ">: ";
 }
 </style>
